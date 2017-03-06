1b88840d12ff3820ca1bce3c0efae79e1a3da94f
Jeremy Bae <jswbae@gmail.com>
2017-03-06 13:14:50 +0900

    Initial Commit

---
+++ startup_mailer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import subprocess
+import smtplib
+import socket
+from email.mime.text import MIMEText
+import datetime
+
+today = datetime.date.today()
+
+# Get Linux IP Address
+arg='ip route list'
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+data = p.communicate()
+split_data = data[0].split()
+ipaddr = split_data[split_data.index('src')+1]
+my_ip = 'Your ip is %s' %  ipaddr
+
+
+# Send E-Mail
+to = 'me@example.com'
+gmail_user = 'test@gmail.com'
+gmail_password = 'yourpassword'
+smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+smtpserver.ehlo
+smtpserver.login(gmail_user, gmail_password)
+msg = MIMEText(my_ip)
+msg['Subject'] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
+msg['From'] = gmail_user
+msg['To'] = to
+smtpserver.sendmail(gmail_user, [to], msg.as_string())
+smtpserver.quit()
